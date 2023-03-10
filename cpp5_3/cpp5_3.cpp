d56b5bf96e11f293f3e8a7925ba0b0aecaa3a58a
cQula <doktorsad@yandex.ru>
2023-03-10 20:50:36 +0300

    correct#1

--- cpp5_3/cpp5_3.cpp
+++ cpp5_3/cpp5_3.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <array>
+#include <string>
 
 class DivideThree {
 private:
-    std::vector<int> var;
-    int sum;
-    int count;
+    int sum = 0;
+    int count = 0;
 public:
     DivideThree() = default;
-    DivideThree(const DivideThree& other) = delete;
-    DivideThree& operator= (const DivideThree& oyher) = delete;
-    void push(const int i) { var.push_back(i); }
-    void operator() () {
-        for (size_t idx_i = 0; idx_i < var.size(); ++idx_i) {
-            if (var[idx_i] % 3 == 0 && var[idx_i] != 0) {
-                ++this->count;
-                this->sum += var[idx_i];
-            }
+    ~DivideThree() = default;
+    void operator() (const int i) {
+        if ((i % 3) == 0) {
+            sum += i;
+            ++count;
         }
     }
     int get_sum()const { return this->sum; }
     int get_count() const { return this->count; }
-    int get_size() const { return this->var.size(); }
-    int get_num(const int idx_i) const { return var[idx_i]; }
-    friend std::ostream &operator<< (std::ostream& out, const DivideThree& var) {
-        for (size_t idx_i = 0; idx_i < var.get_size(); ++idx_i)
-            out << var.get_num(idx_i) << '\t';
-        out << '\n';
-        return out;
+    void out_DivideThree(const std::string &str) const {
+        std::cout << str <<" count: " << get_count() <<
+            " sum: " << get_sum() << std::endl;
     }
 };
 
+template <class T>
+void out_print(const T& var,const std::string &str) {
+    std::cout << str << ": ";
+    for (const auto& i : var)
+        std::cout << i << '\t';
+    std::cout << std::endl;
+}
+
 int main() {
-    int size = 10;
-    DivideThree dt;
-    for (size_t idx_i = 0; idx_i < size; ++idx_i) {
-        dt.push(idx_i);
-    }
-    std::cout << dt;
-    dt();
-    std::cout << dt.get_sum() << '\t' << dt.get_count() << '\n';
+    DivideThree sum_vec;
+    DivideThree sum_arr;
+    std::string str_vec = "vec";
+    std::string str_arr = "arr";
+    std::vector<int> vec = { 1,2,3,4,5,6,7,8,9 };
+    std::array<int, 9> arr = { 11,22,33,44,55,66,77,88,99 };
+    out_print(vec, str_vec);
+    out_print(arr, str_arr);
+    sum_vec = std::for_each(vec.begin(), vec.end(), sum_vec);
+    sum_arr = std::for_each(arr.begin(), arr.end(), sum_arr);
+    sum_vec.out_DivideThree(str_vec);
+    sum_arr.out_DivideThree(str_arr);
 }
